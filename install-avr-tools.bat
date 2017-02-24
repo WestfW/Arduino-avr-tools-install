9001bb4b96a9cfcb1efb94f8fae4089745a16959
WestfW <westfw@gmail.com>
2017-02-24 01:24:41 -0800

    Clean up some; fix commenting bugs (add more comments!) Now tested on W10 (32bit) and WXP

--- install-avr-tools.bat
+++ install-avr-tools.bat
@@ -1,99 +1,168 @@
 @echo off
-if "%1"=="" (
-  :: If we're called without an arg, we're probably going to need to
-  :: explicitly reinvoke cmd.exe, which will prevent us from being able
-  :: to change the current path.  So use a secondary tmp .bat file
-  call :gettempname
-) else (
-  set TMPFILE=%1
+@echo.
+SET DEBUG=REM
+call :clearerrors
+%DEBUG% checking args.  errorlevel=%ERRORLEVEL%
+set TMPFILE=%1
+if "%1" EQU "" (
+  REM If we're called without an arg, we're probably going to need to
+  REM explicitly reinvoke cmd.exe, which will prevent us from being able
+  REM to change the current path.  So use a secondary tmp .bat file
+  call :gettemp
 )
 
-if !SystemDrive! NEQ %SystemDrive% (
-:: We have to run in /V mode to get late evaluation of variables inside loops
-  cmd.exe /V /C %0 %TMPFILE%
+REM See if we're already in Delayed Evaluation mode.
+%DEBUG% checking cmd mode
+if "!x!" NEQ "%x%" (
+  REM We have to run in /V mode to get late evaluation of variables inside loops
+  %DEBUG% re-running in delayed eval mode
+  cmd.exe /V /C "%0" %TMPFILE%
+  %DEBUG% recursion finished.  Errorlevel %ERRORLEVEL%
+  if ERRORLEVEL 1 ( GOTO EOF )
+  REM Assuming it worked, invoke the temp file that was created
+  if not exist %TMPFILE% (
+    %DEBUG% Unexpected error.  No TMPFILE %TMPFILE%
+    exit /b 432
+  )
+  %DEBUG% calling tmpfile
   call %TMPFILE%
+  REM Pretty print some stuff for the user. 
   @echo Checking tool versions
   @echo. 
   avr-gcc --version
   avrdude -v
-  goto :eof
+  REM And we're done (Most of the work being done in the sub-process.)
+  goto eof
 )
-:: Look for avr-gcc in path.
-@echo Looking for avr-gcc in current path
-:: @set ERRORLEVEL=0
-@where avr-gcc >NUL 2>NUL
-IF %ERRORLEVEL% EQU 0 (
-  @ECHO avr-gcc already installed
-  where avr-gcc
+
+REM ======================================================================
+
+REM Here is most of the work of the script.
+REM Look through the various places where an Arduino install is likely to exist,
+REM  make sure that we can find the avr-gcc binaries that should be part of that
+REM  install, print out the version, and ask the user if that's what they want.
+
+REM Look for existing avr-gcc in path.
+%DEBUG% Looking for avr-gcc in current path
+call :clearerrors
+call :which avr-gcc.exe
+IF "%gotwhich%" NEQ "" (
+  @ECHO avr-gcc already installed at %gotwhich%
   avr-gcc --version
-  exit /b 1
+  exit /b 123
 ) ELSE (
   @ECHO No avr-gcc currently installed.
 )
-:: look for Arduino install.
+REM look for Arduino install.
 IF EXIST "%ProgramFiles%\Arduino*" (
   @echo Found at least one Arduino Program
+  @echo.
   FOR /F "tokens=*" %%f IN ('dir /b /x "%ProgramFiles%\Arduino*"') DO (
     SET prg=%ProgramFiles%\%%f
-    @echo Looks like !prg! has version:
+    @echo Looks like !prg! has version 
     call :gccversion "!prg!\Hardware\tools\avr\bin\avr-gcc.exe"
     SET /P confirm="Use !prg! ? [y/n]>"
-::    @ echo got response: !confirm!
     if "!confirm!"=="y" (
-::      echo selected prg = !prg!
+      SET aroot=!prg!
       GOTO gotdir
     )
   )
-  GOTO noarduino
+  REM try some of the more unlikely places that Arduino might live.
+  goto :noarduino
 ) ELSE (
 :noarduino
-  @echo Can't find Arduino prg=%prg%
-  exit /b 1
+  @echo Can't find Arduino
+  exit /b 5678
 )
   
-:: prompt for arduino install location.
+REM prompt for arduino install location.
+@echo ****WHY DID WE GET HERE****
 @echo asking if this is OK
 SET /P confirm="Use %prg% [y/n]>"
 if "%confirm%" NEQ "y" exit /b 0
 
-:gotdir
 
-:: figure out arduino install version.
-:: @echo gotdir has prg = %prg%
+:gotdir
+REM figure out arduino install version.
+%DEBUG% gotdir has prg = %prg%
 IF EXIST "%prg%\hardware\tools\avr\bin\avr-gcc.exe" (
-::  @echo Looks like 1.6+ Install.
-  @echo Found avr-gcc !
+  @echo Found avr-gcc
   set bin=%prg%\hardware\tools\avr\bin
-  for %%i in ("%prg%\hardware\tools\avr\etc") do (
-::    @echo looping
-    set etc=%%~sfi
-  )
+  set etc=%prg%\hardware\tools\avr\etc
 ) else (
-  @echo Can't find a bin directory
-  exit /b 1
+  @echo Cant find a bin directory
+  exit /b 963
 )
-:: find bin directory.
-:: create tentative path for binaries.
-:: check tool versions.
-@echo Setting paths to include bin and etc
-:: setx PATH "%PATH%;%bin%;%etc%"
-:: PATH %PATH%;%bin%;%etc%
+
+%DEBUG% Checking for utils at %prg%\hardware\tools\avr\utils\bin\
+IF EXIST "%aroot%\hardware\tools\avr\utils\bin\make.exe" (
+  REM See if we have make and etc as well (from Arduino 1.0.x/WinAVR)
+  %DEBUG% Found make.exe
+  set utils=%aroot%\hardware\tools\avr\utils\bin
+)
+
+
+REM find bin directory.
+REM create tentative path for binaries and put it in our tmp batch file
+%DEBUG% Setting paths to include bin and etc
+REM setx PATH "%PATH%;%bin%;%etc%"
+REM PATH %PATH%;%bin%;%etc%
 echo PATH %PATH%;%bin%;%etc%>%TMPFILE%
-goto :eof
+if "%utils%" NEQ "" (
+   REM Check for make already installed
+   %DEBUG% Have utils; checking whether make is already installed.
+   call :which make.exe
+   if "%gotwhich%" EQU "" (
+      %DEBUG Adding utils as well at %utils%
+      echo PATH %PATH%;%bin%;%etc%;%utils%>%TMPFILE%
+
+   )
+)
+
+exit /b 0
+goto eof
+
+
+REM ----------------------------------------------------------------------
+REM          Subroutines
+REM ----------------------------------------------------------------------
+
+:which
+    %DEBUG% which %1
+    SET gotwhich=
+    for %%i in (%1) do set fullspec=%%~$PATH:i
+    if not "x!fullspec!"=="x" (
+       %DEBUG% !fullspec!
+       set gotwhich=!fullspec!    
+    )
+    %DEBUG% End which %gotwhich%
+    goto eof
+
 
 :gccversion
-:: This implements "gcc --version | head -1" - show the first line
-if EXIST %1 (
-  FOR /F "delims=*" %%l in ('%1 --version') DO (
-    @echo %%l
-    :: return after we've output one line
-    exit /b 0
-  )
-)
-exit /b 0
+   REM This implements "gcc --version | head -1" - show the first line
+   if EXIST %1 (
+     FOR /F "delims=*" %%l in ('%1 --version') DO (
+       @echo %%l
+       REM return after we've output one line
+       exit /b 0
+     )
+   )
+   exit /b 0
 
-:gettempname
-set TMPFILE=%TMP%\mytempfile-%RANDOM%-%TIME:~6,5%.bat
-if exist "%TMPFILE%" GOTO GETTEMPNAME
-:: goto :eof
+REM ----------------------------------------------------------------------
 
+REM Get a unique temporary filename,
+REM   Thus uses our scriptname, a random number, and the time
+:gettemp
+   set TMPFILE=%TMP%\install-avr-tools-%RANDOM%-%TIME:~6,5%.bat
+   if exist "%TMPFILE%" GOTO gettempname
+   exit /b 0
+
+REM ----------------------------------------------------------------------
+
+REM Clear the ERRORLEVEL to 0, if it happened to be set
+:clearerrors
+   exit /b 0
+
+:eof
